e38befb6d529ebd137a056b9e4796732d9c295b7
Thomas Meichelböck <thomas.meichelboeck@gmail.com>
2025-01-13 21:58:27 +0900

    add pictures and stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./resources/js/*.js", "./*.html"],
+  content: ["./resources/js/*.js", "./*.html", "./resources/pages/**/*.html"],
   theme: {
     extend: {},
   },
